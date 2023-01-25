76d66250f3a402628231a9fffcd87903acf47d95
SonMooSans <god63820869@gmail.com>
2023-01-25 23:46:01 +0800

    Clean code

---
+++ tsup.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+    entry: ["src/index.ts"],
+    splitting: false,
+    sourcemap: "inline",
+    clean: true,
+    minify: true,
+});
