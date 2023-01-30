015509ce0e2cfe5b9a8bf418cf5db707418ca672
SonMooSans <god63820869@gmail.com>
2023-01-30 17:24:14 +0800

    Disable minify

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
     entry: ["src/index.ts"],
-    sourcemap: true,
     clean: true,
-    minify: true,
+    minify: false,
     format: ["esm", "cjs"],
 });
