95c081f51d66e12aa7c91c72760960bbf95f7e5e
SonMooSans <god63820869@gmail.com>
2023-02-25 17:58:26 +0800

    Support lateinit client

--- src/core/init-dfp.ts
+++ src/core/init-dfp.ts
@@ -8,21 +8,27 @@ import {
 } from "./command.js";
 
 export type DfpConfig = {
-    client: Client;
+    client?: Client;
 };
 
 export interface Dfp<Params extends CommandParams> {
     command: CommandBuilder<Params>;
     start(options: StartOptions): Promise<StartResult>;
     loaded: Node[];
+    client: Client | null;
 }
 
 export function initDiscordFP(config: DfpConfig) {
     const dfp: Dfp<{ _ctx: {} }> = {
         command: initCommandBuilder(),
         loaded: [],
+        client: config.client ?? null,
         async start(options) {
-            const result = await start(config.client, options);
+            if (this.client == null) {
+                throw new Error("The client can't be null");
+            }
+
+            const result = await start(this.client, options);
 
             this.loaded = result.loaded;
             return result;
